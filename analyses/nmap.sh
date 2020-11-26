70702f6df4ff17cb695b8e53c8c8cc47fe705431
ljiahao <jh_liu@zju.edu.cn>
2020-11-26 16:48:57 +0800

    add

--- analyses/nmap.sh
+++ analyses/nmap.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# 让脚本遇到错误时停止执行，并指出错误的行数信息。
 set -u
 
+# 判断firmadyne.config 是否存在，存在的话便引用它
 if [ -e ./firmadyne.config ]; then
     source ./firmadyne.config
 elif [ -e ../firmadyne.config ]; then
@@ -11,27 +13,33 @@ else
     exit 1
 fi
 
+# 如果参数个数不为1，给出错误提示。
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <image ID>"
     exit 1
 fi
 
+# IID为脚本第一个参数
 IID=${1}
+# 获取工作目录
 WORK_DIR=`get_scratch ${IID}`
 
 #Nmap options to use for scanning:
 NMAP_OPTS="-v -n -sSV"
 
+# which指令会在环境变量$PATH设置的目录里查找符合条件的文件
 if ! which nmap > /dev/null; then
     echo "[-] missing nmap binary"
     exit 1
 fi
 
+# 判断work dir是否为目录
 if ! [ -d ${WORK_DIR} ]; then
     echo "[-] missing working directory of image ID ${IID}"
     exit 1
 fi
 
+# 判断文件是否为普通文件
 if ! [ -f ${WORK_DIR}/run.sh ]; then
     echo "[-] missing start script (run.sh) of image ID ${IID}"
     exit 1
